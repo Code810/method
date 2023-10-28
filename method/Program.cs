7171f620f71aea7b7eda1e66d3b35747c7f2bd7e
code810 <nadirssh@code.edu.az>
2023-10-28 22:55:14 +0400

    task1 ready

---
+++ method/Program.cs
@@ -0,0 +1,12 @@
+﻿#region task1
+string result(int num)
+{
+    if (num%7==0 && num%3==0)
+    {
+        return "bolunur";
+    }
+    return "bolunmur";
+}
+
+Console.WriteLine(result(14));
+#endregion
